f1e2e04ead7a8bb155820446f1dd40f1029ab91d
Sohail Sayed <snsayed06@gmail.com>
2026-04-06 05:38:37 -0500

    update timestamp

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const makeCommits = (n) => {
   if(n===0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+  const date = moment().subtract(2, "y").add(1, "d").add(x, "w").add(y, "d").format();
 
   const data = {
     date: date,
